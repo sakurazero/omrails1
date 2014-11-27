4d3774a41c093af209ef10f8547ddabcb6b7a1aa
Ramon S. DeBelen <sakurazro@gmail.com>
2014-11-27 10:35:30 +0800

    production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,7 +80,7 @@ Rails.application.configure do
 config.paperclip_defaults = {
   :storage => :s3,
   :s3_credentials => {
-    :bucket => 'gametrade'
+    :bucket => 'gametrade',
     :access_key_id => 'AKIAI5OT76QWVGCTOFCQ',
     :secret_access_key => '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'
   }
